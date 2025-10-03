8fdb834cb1ad3f9d6c9a98ef45c1b69656ef881b
James <91348155+FrogAi@users.noreply.github.com>
2025-10-03 15:12:03 -0700

    Turn Desires

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -4,6 +4,7 @@ from openpilot.common.realtime import DT_MDL
 
 LaneChangeState = log.LaneChangeState
 LaneChangeDirection = log.LaneChangeDirection
+TurnDirection = log.Desire
 
 LANE_CHANGE_SPEED_MIN = 20 * CV.MPH_TO_MS
 LANE_CHANGE_TIME_MAX = 10.
@@ -30,6 +31,11 @@ DESIRES = {
 }
 
 # FrogPilot variables
+TURN_DESIRES = {
+  TurnDirection.none: log.Desire.none,
+  TurnDirection.turnLeft: log.Desire.turnLeft,
+  TurnDirection.turnRight: log.Desire.turnRight,
+}
 
 
 class DesireHelper:
@@ -123,7 +129,12 @@ class DesireHelper:
     self.lane_change_completed &= one_blinker
     self.prev_one_blinker = one_blinker
 
-    self.desire = DESIRES[self.lane_change_direction][self.lane_change_state]
+    if one_blinker and below_lane_change_speed and not carstate.standstill and frogpilot_toggles.use_turn_desires:
+      self.turn_direction = TurnDirection.turnLeft if carstate.leftBlinker else TurnDirection.turnRight
+      self.desire = TURN_DESIRES[self.turn_direction]
+    else:
+      self.turn_direction = TurnDirection.none
+      self.desire = DESIRES[self.lane_change_direction][self.lane_change_state]
 
     # Send keep pulse once per second during LaneChangeStart.preLaneChange
     if self.lane_change_state in (LaneChangeState.off, LaneChangeState.laneChangeStarting):

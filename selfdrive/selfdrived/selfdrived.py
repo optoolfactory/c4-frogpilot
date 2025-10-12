91f7b70e49c65df60f61e0394743c6b1030045b3
James <91348155+FrogAi@users.noreply.github.com>
2025-10-11 18:53:36 -0700

    FrogPilot branding

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -132,7 +132,7 @@ class SelfdriveD:
       self.ignored_processes = {'loggerd', }
 
     # Determine startup event
-    self.startup_event = EventName.startup if build_metadata.openpilot.comma_remote and build_metadata.tested_channel else EventName.startupMaster
+    self.startup_event = FrogPilotEventName.customStartupAlert
     if not car_recognized:
       self.startup_event = EventName.startupNoCar
     elif car_recognized and self.CP.passive:

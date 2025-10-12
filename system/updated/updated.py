18240cab14d87f0da57a56b9bd69aa3c4ca0e1e4
James <91348155+FrogAi@users.noreply.github.com>
2025-10-11 19:35:21 -0700

    FrogPilot Sentry reporting

--- system/updated/updated.py
+++ system/updated/updated.py
@@ -410,6 +410,7 @@ class Updater:
     cloudlog.info("finalize success!")
 
     # FrogPilot variables
+    self.params.put("Updated", datetime.datetime.now().strftime("%B %d, %Y - %I:%M%p"))
 
 def main() -> None:
   params = Params()
@@ -433,10 +434,6 @@ def main() -> None:
     if Path(os.path.join(STAGING_ROOT, "old_openpilot")).is_dir():
       cloudlog.event("update installed")
 
-    if not params.get("InstallDate"):
-      t = datetime.datetime.now(datetime.UTC).replace(tzinfo=None)
-      params.put("InstallDate", t)
-
     updater = Updater()
     update_failed_count = 0 # TODO: Load from param?
     wait_helper = WaitTimeHelper()
@@ -452,6 +449,8 @@ def main() -> None:
 
     # FrogPilot variables
     params_memory = Params(memory=True)
+
+    install_date_set = params.get("InstallDate") is not None
 
     while True:
       wait_helper.ready_event.clear()
@@ -473,6 +472,9 @@ def main() -> None:
           continue
 
         # FrogPilot variables
+        if not install_date_set:
+          params.put("InstallDate", datetime.datetime.now().strftime("%B %d, %Y - %I:%M%p"))
+          install_date_set = True
 
         update_failed_count += 1
 

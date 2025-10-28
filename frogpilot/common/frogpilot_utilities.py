f4b5747f5c7ea26b5a869479ffe12097015eea84
James <91348155+FrogAi@users.noreply.github.com>
2025-10-28 02:21:36 -0700

    Custom Themes

--- frogpilot/common/frogpilot_utilities.py
+++ frogpilot/common/frogpilot_utilities.py
@@ -17,6 +17,7 @@ running_threads = {}
 
 locks = {
   "backup_toggles": threading.Lock(),
+  "download_theme": threading.Lock(),
   "update_checks": threading.Lock(),
   "update_openpilot": threading.Lock(),
 }

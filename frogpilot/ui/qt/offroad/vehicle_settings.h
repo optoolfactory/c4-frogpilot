killinen/openpilot

FrogPilot panel

frogpilot/ui/qt/offroad/vehicle_settings.h
@@ -1,0 +1,39 @@
+#pragma once
+
+#include "frogpilot/ui/qt/offroad/frogpilot_settings.h"
+
+class FrogPilotVehiclesPanel : public FrogPilotListWidget {
+  Q_OBJECT
+
+public:
+  explicit FrogPilotVehiclesPanel(FrogPilotSettingsWindow *parent);
+
+signals:
+  void openSubPanel();
+
+protected:
+  void showEvent(QShowEvent *event) override;
+
+private:
+  void updateState(const UIState &s);
+  void updateToggles();
+
+  bool forceOpenDescriptions;
+  bool started;
+
+  std::map<QString, AbstractControl*> toggles;
+
+  QSet<QString> gmKeys = {"ExperimentalGMTune", "VoltSNG"};
+  QSet<QString> hkgKeys = {"TacoTuneHacks"};
+  QSet<QString> longitudinalKeys = {"ExperimentalGMTune", "FrogsGoMoosTweak", "SNGHack", "VoltSNG"};
+  QSet<QString> toyotaKeys = {"ClusterOffset", "FrogsGoMoosTweak", "LockDoorsTimer", "SNGHack", "ToyotaDoors"};
+  QSet<QString> vehicleInfoKeys = {"BlindSpotSupport", "HardwareDetected", "OpenpilotLongitudinal", "PedalSupport", "RadarSupport", "SDSUSupport", "SNGSupport"};
+
+  QSet<QString> parentKeys;
+
+  FrogPilotSettingsWindow *parent;
+
+  ParamControl *disableOpenpilotLong;
+
+  Params params;
+};

optoolfactory/c4-frogpilot

frogpilot/ui/qt/offroad/vehicle_settings.h
@@ -1,0 +1,40 @@
+#pragma once
+
+#include "frogpilot/ui/qt/offroad/frogpilot_settings.h"
+
+class FrogPilotVehiclesPanel : public FrogPilotListWidget {
+  Q_OBJECT
+
+public:
+  explicit FrogPilotVehiclesPanel(FrogPilotSettingsWindow *parent);
+
+signals:
+  void openSubPanel();
+
+protected:
+  void showEvent(QShowEvent *event) override;
+
+private:
+  void updateState(const UIState &s);
+  void updateToggles();
+
+  bool forceOpenDescriptions;
+  bool started;
+
+  std::map<QString, AbstractControl*> toggles;
+
+  QSet<QString> gmKeys = {"VoltSNG"};
+  QSet<QString> hkgKeys = {"TacoTuneHacks"};
+  QSet<QString> longitudinalKeys = {"FrogsGoMoosTweak", "SNGHack", "SubaruSNG", "VoltSNG"};
+  QSet<QString> subaruKeys = {"SubaruSNG"};
+  QSet<QString> toyotaKeys = {"ClusterOffset", "FrogsGoMoosTweak", "LockDoorsTimer", "SNGHack", "ToyotaDoors"};
+  QSet<QString> vehicleInfoKeys = {"BlindSpotSupport", "HardwareDetected", "OpenpilotLongitudinal", "PedalSupport", "RadarSupport", "SDSUSupport", "SNGSupport"};
+
+  QSet<QString> parentKeys;
+
+  FrogPilotSettingsWindow *parent;
+
+  ParamControl *disableOpenpilotLong;
+
+  Params params;
+};
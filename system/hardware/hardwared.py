043d5fa305d884805931efc77816d0e4e7664fae
James <91348155+FrogAi@users.noreply.github.com>
2026-02-15 06:05:22 -0700

    Temp fix

--- system/hardware/hardwared.py
+++ system/hardware/hardwared.py
@@ -337,7 +337,7 @@ def hardware_thread(end_event, hw_queue) -> None:
     if not PC:
       # we enforce this for our software, but you are welcome
       # to make a different decision in your software
-      startup_conditions["registered_device"] = PC or (params.get("DongleId") != UNREGISTERED_DONGLE_ID)
+      startup_conditions["registered_device"] = True
 
     # Handle offroad/onroad transition
     should_start = all(onroad_conditions.values())
